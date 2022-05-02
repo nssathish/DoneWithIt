b119be5075ef01fed393686b499ad93acacd3ad3
Sathish Kumar Subramanian <nsubramanian.sathish@gmail.com>
2022-05-02 11:59:12 +0530

    CodewithMosh Youtube: Some changes i need to work on

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
+  const handlePress = () => {
+    console.log("Text pressed")
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>Let the hacking begin. It's a famous quote from the movie social networking that is loosely based on Facebook's history</Text>
+      <Image source={{
+        height: 300,
+        width: 200,
+        uri: "https://picsum.photos/200/300"
+        }} />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
